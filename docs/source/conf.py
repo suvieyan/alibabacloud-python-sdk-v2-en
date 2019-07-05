0e059c1b9675051fb9cbf86690aa5c45f0fd2e05
wb-yxy487231 <wb-yxy487231@alibaba-inc.com>
2019-07-05 18:42:01 +0800

    modify config.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,7 +39,7 @@ extensions = ['sphinx.ext.autodoc',
 
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+# templates_path = ['_templates']
 
 source_suffix = '.rst'
 
@@ -67,6 +67,6 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 
